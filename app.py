a8651e454f1ef4d72739abbcb7e6adb7f754f26b
tinzaza <74438818+tinzaza@users.noreply.github.com>
2026-01-12 02:30:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -572,5 +572,6 @@ def logout():
     session.clear()
     return redirect(url_for("login"))
 
-if __name__=="__main__":
-    app.run(debug=True,port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
+
